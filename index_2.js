537f15e2d54c2832d614d071e9d04379c2589528
JaninWanjiru <janinndongo02@gmail.com>
2025-06-07 15:05:00 +0300

    complete challenge 2 object to array

--- index_2.js
+++ index_2.js
@@ -1,3 +1,4 @@
+// Challenge 1 Min of negatives
 function minOfNegatives(arr) {
     let smallNegNo = null;
     for (let i=0; i<arr.length; i++) {
@@ -12,3 +13,42 @@ function minOfNegatives(arr) {
 }
 console.log(minOfNegatives([3, -2, -8, 4, -5]));
 console.log(minOfNegatives([1, 3, 5]));
+
+
+// Challenge 2 Object to array
+function objectToArray(obj) {
+    let result = [];
+    let keys = Object.keys(obj);
+
+    for (let i=0; i<keys.length; i++) {
+        let key = keys[i];    //key = "name" then "age"
+        let value =obj[key];  //value= "Alice" then 25
+        result.push(value);   //returns only the value (not key-value pairs)
+    }
+    return result;
+}
+console.log(objectToArray({name: "Alice", age: 25}))
+
+
+// Challenge 3 Capitalize words
+
+
+// Challenge 4 Find duplicates
+
+
+// Challenge 5 Group by Type
+
+
+// Challenge 6 Merge arrays Uniquely
+
+
+// Challenge 7 Object Key formatter
+
+
+// Challenge 8 Remove falsy values
+
+
+// Challenge 9 Second largest
+
+
+// Challenge 10 Sum nested arrays
